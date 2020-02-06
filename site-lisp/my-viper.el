a975eb1330ff81056788408b1c42c23390e8be52
tslight <tslight@pm.com>
2020-02-06 19:56:52 +0000

    add Viper settings

---
+++ site-lisp/my-viper.el
@@ -0,0 +1,15 @@
+;;; my-viper.el --- My Viper Settings
+
+;;; Commentary:
+
+;; Copyright (C) 2019
+;; Author:  Toby Slight
+
+;;; Code:
+;; -*- lexical-binding: t; -*-
+(setq viper-inhibit-startup-message 't)
+(setq viper-expert-level '5)
+(setq viper-mode t)
+(require 'viper)
+(provide 'my-viper)
+;;; my-viper.el ends here

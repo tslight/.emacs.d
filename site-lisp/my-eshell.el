d242092b08c63d878e434e257d787340b8cc14ef
tslight <tslight@pm.com>
2020-05-10 23:02:39 +0100

    already initialized

--- site-lisp/my-eshell.el
+++ site-lisp/my-eshell.el
@@ -58,10 +58,11 @@ With ARG also open the directory in a `dired' buffer."
 (setq eshell-hist-ignoredups t)
 (setq eshell-save-history-on-exit t)
 
-(require 'em-smart)
+;; smart stuff
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
+;; (eshell-smart-initialize)
 
 (setq eshell-prefer-lisp-functions t)
 (setq eshell-prefer-lisp-variables t)

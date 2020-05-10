4a63c49ec1bacda0bf7adda697ffa4e8d0470397
tslight <tslight@pm.com>
2020-05-10 17:17:53 +0100

    better prompt

--- site-lisp/my-eshell.el
+++ site-lisp/my-eshell.el
@@ -13,35 +13,6 @@
 (require 'eshell)
 (require 'em-smart)
 
-;; https://www.emacswiki.org/emacs/EshellAlias
-(defun my/bash-to-eshell-aliases ()
-  "Convert bash aliases to eshell aliases."
-  (interactive)
-  (progn
-    (message "Parsing aliases")
-    (shell-command "alias" "bash-aliases" "bash-errors")
-    (switch-to-buffer "bash-aliases")
-    (replace-match "alias " "")
-    (goto-char 1)
-    (replace-match "='" " ")
-    (goto-char 1)
-    (replace-match "'\n" "\n")
-    (goto-char 1)
-    (let ((alias-name) (command-string) (alias-list))
-      (while (not (eobp))
-        (while (not (char-equal (char-after) 32))
-          (forward-char 1))
-        (setq alias-name
-              (buffer-substring-no-properties (line-beginning-position) (point)))
-        (forward-char 1)
-        (setq command-string
-              (buffer-substring-no-properties (point) (line-end-position)))
-        (setq alias-list (cons (list alias-name command-string) alias-list))
-        (forward-line 1))
-      (setq eshell-command-aliases-list alias-list))
-    (if (get-buffer "bash-aliases")(kill-buffer "bash-aliases"))
-    (if (get-buffer "bash-errors")(kill-buffer "bash-errors"))))
-
 (setq eshell-history-size 2048)
 (setq eshell-where-to-jump 'begin)
 (setq eshell-review-quick-commands nil)
@@ -55,7 +26,8 @@
          (user-login-name) "@" (system-name) " "
          (if (string= (eshell/pwd) (getenv "HOME"))
              "~" (eshell/basename (eshell/pwd)))
-         (if (= (user-uid) 0) " # " " $ "))))
+         "\n"
+         (if (= (user-uid) 0) "# " "$ "))))
 
 (my/bind-always "C-c e s" eshell)
 

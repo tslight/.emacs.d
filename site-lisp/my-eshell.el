1102f1c1f4ba12630cc2cf49dd16d951a3990f94
tslight <tslight@pm.com>
2020-05-10 22:36:44 +0100

    better colors

--- site-lisp/my-eshell.el
+++ site-lisp/my-eshell.el
@@ -96,5 +96,8 @@ With ARG also open the directory in a `dired' buffer."
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
 
+;; (add-hook 'eshell-preoutput-filter-functions 'ansi-color-filter-apply)
+(add-hook 'eshell-preoutput-filter-functions 'ansi-color-apply)
+
 (provide 'my-eshell)
 ;;; my-eshell.el ends here

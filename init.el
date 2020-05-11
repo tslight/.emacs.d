85f09faf5122af5542e0abf624460aa7288cfe57
tslight <tslight@pm.com>
2020-05-11 08:17:32 +0100

    get rid of all package configuration for a simpler setup if necessary

--- init.el
+++ init.el
@@ -12,35 +12,13 @@
       (gc-cons-threshold most-positive-fixnum)
       (gc-cons-percentage 0.8))
 
-  ;; setup package.el
-  (require 'package)
-  (setq package-enable-at-startup nil)
-  (setq package--init-file-ensured t)
-  (setq package-archives
-        '(("melpa" . "https://melpa.org/packages/")
-          ("gnu" . "https://elpa.gnu.org/packages/")))
-  (package-initialize)
-  
   (byte-recompile-file (concat user-emacs-directory "keys.el") 'nil 0 t)
   (add-to-list 'load-path (concat user-emacs-directory "site-lisp/"))
   (byte-recompile-directory (concat user-emacs-directory "site-lisp") 0)
   (mapc (lambda (file) (load file))
         (directory-files (concat user-emacs-directory "site-lisp") t "\.elc$"))
 
-  ;; install use-package
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-
-  ;; `use-package' does all the heavy lifting in my config.
-  (setq use-package-enable-imenu-support t)
-  ;; (setq use-package-always-ensure t)
-  (setq use-package-verbose t)
-  (require 'use-package)
-  (require 'bind-key)
-
   (byte-recompile-file (concat user-emacs-directory "init.el") 'nil 0 'nil)
-  (byte-recompile-file (concat user-emacs-directory "use.el") 'nil 0 t)
   (message "Hacks and glory await! :-)"))
 
 (provide 'init)
---
+++ site-lisp/my-ido.el
@@ -0,0 +1,77 @@
+(require 'ido)
+
+(setq ido-use-virtual-buffers 't) ;; show recent files too
+
+(setq ido-create-new-buffer 'always)
+
+(setq ido-enable-prefix t)
+
+(setq ido-enable-flex-matching 't)
+
+(setq ido-auto-merge-work-directories-length -1)
+
+;; (setq ido-use-filename-at-point 'ffap-guesser)
+(setq ido-use-filename-at-point 'nil)
+
+(setq ido-decorations (quote ("\n--> "
+                              " "
+                              "\n    "
+                              "\n    ..."
+                              "["  "]"
+                              "  [No match]"
+                              "  [Matched]"
+                              "  [Not readable]"
+                              "  [Too big]"
+                              "  [Confirm]")))
+
+(require 'icomplete)
+(setq icomplete-separator "\n")
+(setq icomplete-hide-common-prefix 'nil)
+(setq icomplete-in-buffer t)
+
+(defun my/ido-find-recentf ()
+  "Use `ido-completing-read' to `find-file' a recent file."
+  (interactive)
+  (recentf-mode 1)
+  (if (find-file (ido-completing-read "Recent: " recentf-list))
+      (message "Opening file...")
+    (message "Aborting")))
+
+(defun my/ido-kill-ring ()
+  "Use `ido-completing-read' to choose a `yank' from the `kill-ring'."
+  (interactive)
+  (let ((my-yank (ido-completing-read "Kill Ring: " kill-ring)))
+    (insert my-yank)))
+
+(defun my/ido-keys ()
+  "Custom ido keybindings."
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+
+(my/bind-always "C-c r" my/ido-find-recentf)
+(my/bind-always "C-M-y" my/ido-kill-ring)
+(my/bind-always "M-x" (lambda () (interactive)
+                 (call-interactively
+                  (intern
+                   (ido-completing-read
+                    "M-x "
+                    (all-completions "" obarray 'commandp))))))
+
+(define-key icomplete-minibuffer-map (kbd "C-f") 'icomplete-forward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-b") 'icomplete-backward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-n") 'icomplete-forward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-p") 'icomplete-backward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-s") 'icomplete-forward-completions)
+(define-key icomplete-minibuffer-map (kbd "C-r") 'icomplete-backward-completions)
+
+(add-hook 'ido-minibuffer-setup-hook (lambda () (set (make-local-variable 'truncate-lines) nil)))
+(add-hook 'ido-setup-hook (lambda () (define-key ido-completion-map [tab] 'ido-complete)))
+(add-hook 'ido-setup-hook 'my/ido-keys)
+
+(icomplete-mode 1)
+(ido-mode 1)
+
+(ido-everywhere 1)
+
+;; stop ido suggestion when doing a save-as
+(define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)

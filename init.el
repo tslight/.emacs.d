e7229e9ddd2cddb86132b1e4f63b13d5fec5500c
tslight <tslight@pm.com>
2020-12-14 21:37:21 +0000

    aesthetics

--- init.el
+++ init.el
@@ -12,13 +12,13 @@
       (gc-cons-threshold most-positive-fixnum)
       (gc-cons-percentage 0.8))
 
+  (add-to-list 'load-path (concat user-emacs-directory "site-lisp/"))
+
   (mapc (lambda (directory) (byte-recompile-directory (concat user-emacs-directory directory) 0))
         '("early-init.d" "init.d" "site-lisp")) ;; compile these directories
 
   (mapc (lambda (file) (byte-recompile-file (concat user-emacs-directory file) 'nil 0 'nil))
         '("early-init.el" "init.el")) ;; compile these files
-
-  (add-to-list 'load-path (concat user-emacs-directory "site-lisp/"))
 
   (mapc (lambda (file) (load file)) ;; load all files in ~/.emacs.d/init.d
         (directory-files (concat user-emacs-directory "init.d") t "^.*\.elc$"))

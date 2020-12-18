de6192ef4f46bc6940849003047963ff517fd1a1
tslight <tslight@pm.me>
2020-12-18 21:11:02 +0000

    add comment

--- early-init.el
+++ early-init.el
@@ -17,6 +17,7 @@
 (setq package-enable-at-startup t)
 ;; Allow loading from the package cache.
 (setq package-quickstart t)
+;; Don't write to my init file!
 (setq package--init-file-ensured t)
 
 (setq package-archives

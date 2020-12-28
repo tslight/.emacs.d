1778f7e3299c6db7e1a701189be1efdbc2ba82bd
tslight <tslight@pm.com>
2020-12-28 12:45:00 +0000

    use-package indentation

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+;;; early-init.el --- early-init  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Copyright: (C) 2020 Toby Slight
+;; Author: Toby Slight <tslight@pm.me>
+
+;;; Code:
 ;; This must be true otherwise use-package won't load!
 (setq package-enable-at-startup t)
 ;; Allow loading from the package cache.
@@ -8,3 +16,10 @@
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
         ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(provide 'early-init)
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; byte-compile-warnings: (not free-vars noruntime)
+;; End:
+;;; early-init.el ends here
